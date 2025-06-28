6ba93daf6b455477abfb270b8a1031d713cb583c
Kerwin <tkoleo84119@gmail.com>
2025-06-29 01:39:09 +0800

    add feature to catch book path from user

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+import sys
+
 from stats import get_num_words, get_chars_dict, chars_dict_to_sorted_list
 
 def main ():
-  book_path = "books/frankenstein.txt"
+  book_path = get_path()
   text = get_book_text(book_path)
   num_words = get_num_words(text)
   chars_dict = get_chars_dict(text)
   chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
   print_report(book_path, num_words, chars_sorted_list)
+
+def get_path ():
+  args = sys.argv
+
+  if len(args) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+  return args[1]
 
 def get_book_text (path):
   with open(path, 'r', encoding='utf-8') as f:
