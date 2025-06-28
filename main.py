543e6cc93a3ecf11ff2a96a238677bec29f0151b
Kerwin <tkoleo84119@gmail.com>
2025-06-29 01:03:07 +0800

    add count_alpha function

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from stats import count_words
+from stats import count_words, count_alpha
 
 def get_book_text (path):
   with open(path, 'r', encoding='utf-8') as f:
     return f.read()
 
 def main ():
-  num_words = count_words(get_book_text('./books/frankenstein.txt'))
+  book = get_book_text('./books/frankenstein.txt')
+  num_words = count_words(book)
+  alpha_dic = count_alpha(book)
   print(f"{num_words} words found in the document")
+  print(alpha_dic)
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,13 @@
 def count_words (text):
   return len(text.split())
+
+def count_alpha (text):
+  alpha_dic = {}
+
+  for alpha in text.lower():
+    if alpha in alpha_dic:
+      alpha_dic[alpha] += 1
+    else:
+      alpha_dic[alpha] = 1
+
+  return alpha_dic
